f7921393ec635ce2dd0bf604646aa0e41f306d6f
JoelRiveraL <joelale033@gmail.com>
2024-12-03 10:15:10 -0500

    new interface

--- lib/logic/dis_eucl_calculator.dart
+++ lib/logic/dis_eucl_calculator.dart
@@ -10,13 +10,11 @@ class DistanceCalculator {
     return 'Distance: ${distance.toStringAsFixed(2)}';
   }
 
-  // Método para calcular la distancia euclidiana
   DistanceCalculator calculateDistance(int x1, int y1, int x2, int y2) {
     double result = _euclideanDistance(x1, y1, x2, y2);
     return DistanceCalculator(distance: result);
   }
 
-  // Lógica del cálculo
   double _euclideanDistance(int x1, int y1, int x2, int y2) {
     x1 = x1 - x2;
     y1 = y1 - y2;
--- lib/screens/distancia_euclidiana_calculated.dart
+++ lib/screens/distancia_euclidiana_calculated.dart
@@ -33,32 +33,126 @@ class _CalcularDistanciaEucState extends State<CalcularDistanciaEuc> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Calcular Distancia Euclidiana'),
+        title: const Text(
+          'Calcular Distancia',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: const Color.fromARGB(255, 58, 183, 64),
+        elevation: 0,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Coordenadas:',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 68, 183, 58),
+              Color.fromARGB(255, 64, 76, 251)
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    blurRadius: 10,
+                    spreadRadius: 5,
+                  ),
+                ],
+              ),
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Text(
+                    'Resultado del Cálculo',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 73, 183, 58),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  _buildInfoText('Punto 1:', '(${widget.x1}, ${widget.y1})'),
+                  const SizedBox(height: 10),
+                  _buildInfoText('Punto 2:', '(${widget.x2}, ${widget.y2})'),
+                  const SizedBox(height: 20),
+                  const Text(
+                    'Distancia Euclidiana',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 58, 183, 64),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    distanceCalculator.distance.toStringAsFixed(2),
+                    style: const TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 64, 76, 251),
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                          const Color.fromARGB(255, 25, 134, 43)),
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      )),
+                      padding: MaterialStateProperty.all(
+                        const EdgeInsets.symmetric(
+                            horizontal: 40, vertical: 15),
+                      ),
+                    ),
+                    child: const Text(
+                      'Regresar',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Text('Punto 1: (${widget.x1}, ${widget.y1})',
-                style: const TextStyle(fontSize: 18)),
-            Text('Punto 2: (${widget.x2}, ${widget.y2})',
-                style: const TextStyle(fontSize: 18)),
-            const SizedBox(height: 20),
-            const Text(
-              'Distancia Euclidiana:',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            Text(
-              distanceCalculator.distance.toStringAsFixed(2),
-              style: const TextStyle(fontSize: 18),
-            ),
-          ],
+          ),
         ),
       ),
+    );
+  }
+
+  Widget _buildInfoText(String label, String value) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          label,
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Color.fromARGB(255, 73, 183, 58),
+          ),
+        ),
+        Text(
+          value,
+          style: const TextStyle(
+            fontSize: 18,
+            color: Color.fromARGB(255, 64, 76, 251),
+          ),
+        ),
+      ],
     );
   }
 }
